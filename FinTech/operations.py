abdf46d10f8a735c35ffac9c6471f1ef2950c4d1
GBDM97 <ghian.m@gmail.com>
2023-06-18 14:30:57 -0300

    working in the find first ref channel mechanism

--- FinTech/operations.py
+++ FinTech/operations.py
@@ -9,9 +9,34 @@ with open(file_path, "r") as file:
     for line in enumerate(file):
         dataArray.append(line[1].replace('"','').split())
 
-def fimatheChannel(day):
+def fimatheChannel(dayInitIndex):
+    
+    #getting the four cds all values:
+    cdsData = [dataArray[dayInitIndex][2],dataArray[dayInitIndex][3],dataArray[dayInitIndex][4],
+    dataArray[dayInitIndex][5],
+    dataArray[dayInitIndex+1][2],dataArray[dayInitIndex+1][3],dataArray[dayInitIndex+1][4],
+    dataArray[dayInitIndex+1][5],
+    dataArray[dayInitIndex+2][2],dataArray[dayInitIndex+2][3],dataArray[dayInitIndex+2][4],
+    dataArray[dayInitIndex+2][5],
+    dataArray[dayInitIndex+3][2],dataArray[dayInitIndex+3][3],dataArray[dayInitIndex+3][4],
+    dataArray[dayInitIndex+3][5]]
+
+    #finding refs and size of the best channel:
+    bestChannelFound = None
     averageChannel = 30
-    print(day)
+    for i in len(cdsData):
+        firstComparedElement = cdsData[i]
+        for ii in range(1, len(cdsData)):
+            secondComparedElement = cdsData[ii]
+            currentAnalyzedChannelSize = abs(firstComparedElement-secondComparedElement)
+            if abs(currentAnalyzedChannelSize - averageChannel) < bestChannelFound:
+                bestChannelFound = sorted([firstComparedElement, secondComparedElement], reverse=True)
+                bestChannelFound.append(bestChannelFound[0]-bestChannelFound[1])
+    #finding the first reference channel:
+    closeOfCd4 = dataArray[dayInitIndex+3][5]
+    
+
+    print(cdsData)
 
 def getDay():
     global currentDate
