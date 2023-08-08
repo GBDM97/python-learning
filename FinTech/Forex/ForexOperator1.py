f6f7b16ebc4531c7979c2df21a27c164078e5707
Ghiancarlo Morelli <ghiancarlo.morelli.externo@caixavidaeprevidencia.com.br>
2023-08-08 19:55:24 -0300

    app directions defined

--- FinTech/Forex/ForexOperator1.py
+++ FinTech/Forex/ForexOperator1.py
@@ -1,1 +1,6 @@
-dimensionsInput = []
+#Next possible dimensions implementations: 
+#Trailing stop distance from current price in case of tp SURF
+#Current price distance to put stop on break even
+#Daily, weekly or monthly Risk Management
+dimensionsInput = [tpType, stoplossMultiplier, breakEvenDistance]
+targetVariable = 0 #total profit over residuals variance arround the regression line
