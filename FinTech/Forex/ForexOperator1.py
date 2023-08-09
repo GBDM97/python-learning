21c2cd9784cea1e5035199647cc4bc618fa143a8
Ghiancarlo Morelli <ghiancarlo.morelli.externo@caixavidaeprevidencia.com.br>
2023-08-09 10:43:39 -0300

    build op1 base

--- FinTech/Forex/ForexOperator1.py
+++ FinTech/Forex/ForexOperator1.py
@@ -2,5 +2,28 @@
 #Trailing stop distance from current price in case of tp SURF
 #Current price distance to put stop on break even
 #Daily, weekly or monthly Risk Management
-dimensionsInput = [tpType, stoplossMultiplier, breakEvenDistance]
+#dimensionsInput = [tpType, stoplossMultiplier, breakEvenDistance]
+import os
+import datetime
+
+cdir = os.getcwd()
+file_path = cdir + "\\FinTech\\Forex\\USDJPY_M15_2023_3Y.csv"
+allResults = {}
+dataArray = []
+with open(file_path, "r") as file:
+    for line in enumerate(file):
+        dataArray.append(line[1].replace('"','').split())
 targetVariable = 0 #total profit over residuals variance arround the regression line
+currentEntryPoint = 0
+
+
+def nextEntryPoint():
+    global currentEntryPoint
+    currentEntryPoint += 1
+    return currentEntryPoint
+nextEntryPoint()
+
+def searchOperation():
+    for i in dataArray:
+        print(i)
+searchOperation()
