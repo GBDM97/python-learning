8cbf09b9f888cdc0de75f7ab38983dbe7174e557
Ghiancarlo Morelli <ghiancarlo.morelli.externo@caixavidaeprevidencia.com.br>
2023-08-10 11:11:31 -0300

    sl functionality is working

--- FinTech/Forex/ForexOperator1.py
+++ FinTech/Forex/ForexOperator1.py
@@ -4,7 +4,8 @@
 #Daily, weekly or monthly Risk Management
 #dimensionsInput = [tpType, stoplossMultiplier, breakEvenDistance]
 import os
-import datetime
+import math
+from datetime import datetime, timedelta
 
 targetVariable = 0 #total profit over residuals variance arround the regression line
 stopLossMultiplier = 2.25
@@ -16,18 +17,47 @@ entryPoints = []
 e = 0 #current entry point index
 m = 0 #curent market candle index
 currentOperation = []
+microChannelSize = 0
+
+def appendOHLCAndDatetime(pl):
+    out = []
+    date_time = datetime(
+        int(pl[0].split(".")[0]),
+        int(pl[0].split(".")[1]),
+        int(pl[0].split(".")[2]),
+        int(pl[1].split(":")[0]),
+        int(pl[1].split(":")[1]),
+        int(pl[1].split(":")[2])
+    )
+    out.append(date_time)
+    if pl[-1] != "buy" and pl[-1] != "sell":
+        out.append(float(pl[2]))
+        out.append(float(pl[3]))
+        out.append(float(pl[4]))
+        out.append(float(pl[5]))
+    else:
+        out.append(float(pl[2]))
+        out.append(float(pl[3]))
+        out.append(None)
+        out.append(None)
+        out.append(pl[4])
+    return out
 
 cdir = os.getcwd()
 file_path = cdir + "\\FinTech\\Forex\\USDJPY_M15_2023_3Y.csv"
 with open(file_path, "r") as file:
     for index, line in enumerate(file):
         if index > 0:
-            marketInfo.append(line.replace('"','').split())
+            pl = line.replace('"','').split()
+            marketInfo.append(appendOHLCAndDatetime(pl))
 file_path = cdir + "\\FinTech\\Forex\\Forex_Entrypoints.csv"
 with open(file_path, "r") as file:
     for index, line in enumerate(file):
+        if index == 0:
+            microChannelSize = float(line.split("=")[1])
         if index > 0:
-            entryPoints.append(line.replace('\n','').replace(' ',',').split(','))        
+            pl = line.replace('\n','').replace(' ',',').split(',')
+            entryPoints.append(appendOHLCAndDatetime(pl))
 
 def nextEntryPoint():
     global e
@@ -36,12 +66,23 @@ def nextEntryPoint():
 
 def searchOperation():
     global m
-    while entryPoints[e][0] != marketInfo[m][0] or entryPoints[e][1].split(":")[0] > marketInfo[m][1].split(":")[0] or entryPoints[e][1].split(":")[1] >= marketInfo[m][1].split(":")[1]:
+    while entryPoints[e][0] >= marketInfo[m][0]:
         m+=1
     enterOperation()
 
 def enterOperation():
-    currentOperation = entryPoints[e] #todo define stop and gain prices
-    
+    currentOperation = entryPoints[e] #dateTime, entryPointRefPrice, entryPointActualPrice, SL, TP, Side
+    if currentOperation[-1] == 'buy':
+        slippage = math.floor(currentOperation[2] - currentOperation[1])
+        sl = currentOperation[1] - microChannelSize*stopLossMultiplier + slippage
+        currentOperation[3] = sl
+            
+    if currentOperation[-1] == 'sell':
+        slippage = math.floor(currentOperation[1] - currentOperation[2])
+        sl = currentOperation[1] + microChannelSize*stopLossMultiplier - slippage
+        currentOperation[3] = sl
+        
+    print(currentOperation)
+    print(marketInfo[m])
     
 searchOperation()
