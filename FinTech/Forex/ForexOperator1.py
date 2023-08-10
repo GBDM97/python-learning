829012604e1c8873b53d8ba4ebfa15229561e6f8
Ghiancarlo Morelli <ghiancarlo.morelli.externo@caixavidaeprevidencia.com.br>
2023-08-10 14:53:48 -0300

    buy trailing stop manager is ready to test

--- FinTech/Forex/ForexOperator1.py
+++ FinTech/Forex/ForexOperator1.py
@@ -2,14 +2,14 @@
 #Trailing stop distance from current price in case of tp SURF
 #Current price distance to put stop on break even
 #Daily, weekly or monthly Risk Management
-#dimensionsInput = [tpType, stoplossMultiplier, breakEvenDistance]
+#dimensionsInput = [tpType, stoplossMultiplier, minBreakEvenDistance]
 import os
 import math
 from datetime import datetime, timedelta
 
 targetVariable = 0 #total profit over residuals variance arround the regression line
 stopLossMultiplier = 2.25
-breakEvenDistance = 1
+minBreakEvenDistance = 1
 
 allResults = {}
 marketInfo = []
@@ -17,9 +17,10 @@ entryPoints = []
 e = 0 #current entry point index
 m = 0 #curent market candle index
 currentOperation = []
-microChannelSize = 0
+miSize = 0
+stopPosition = 0
 
-def appendOHLCAndDatetime(pl):
+def appendInfo(pl):
     out = []
     date_time = datetime(
         int(pl[0].split(".")[0]),
@@ -49,15 +50,15 @@ with open(file_path, "r") as file:
     for index, line in enumerate(file):
         if index > 0:
             pl = line.replace('"','').split()
-            marketInfo.append(appendOHLCAndDatetime(pl))
+            marketInfo.append(appendInfo(pl))
 file_path = cdir + "\\FinTech\\Forex\\Forex_Entrypoints.csv"
 with open(file_path, "r") as file:
     for index, line in enumerate(file):
         if index == 0:
-            microChannelSize = float(line.split("=")[1])
+            miSize = float(line.split("=")[1])
         if index > 0:
             pl = line.replace('\n','').replace(' ',',').split(',')
-            entryPoints.append(appendOHLCAndDatetime(pl))
+            entryPoints.append(appendInfo(pl))
 
 def nextEntryPoint():
     global e
@@ -74,15 +75,47 @@ def enterOperation():
     currentOperation = entryPoints[e] #dateTime, entryPointRefPrice, entryPointActualPrice, SL, TP, Side
     if currentOperation[-1] == 'buy':
         slippage = math.floor(currentOperation[2] - currentOperation[1])
-        sl = currentOperation[1] - microChannelSize*stopLossMultiplier + slippage
+        sl = currentOperation[1] - miSize*stopLossMultiplier + slippage
         currentOperation[3] = sl
+        currentOperation[1] += slippage
             
     if currentOperation[-1] == 'sell':
         slippage = math.floor(currentOperation[1] - currentOperation[2])
-        sl = currentOperation[1] + microChannelSize*stopLossMultiplier - slippage
+        sl = currentOperation[1] + miSize*stopLossMultiplier - slippage
         currentOperation[3] = sl
-        
-    print(currentOperation)
-    print(marketInfo[m])
+        currentOperation[1] -= slippage
+
+    waitForPositionClose()
+
+def waitForPositionClose():
+    global stopPosition
+    if currentOperation[-1] == "buy":
+        while marketInfo[m][-1] > currentOperation[3]: #implement different logic when having more tp types
+            m+=1
+            manageTrailingStop()
+    if currentOperation[-1] == "sell":
+        while marketInfo[m][-1] < currentOperation[3]:
+            m+=1
+            manageTrailingStop()
+
+def manageTrailingStop():
+    firstCdPastLine = False
+    cdPastLineIndex = None
+    if currentOperation[-1] == "buy":
+        if int(marketInfo[m][-1]) > currentOperation[2] + miSize*minBreakEvenDistance and stopPosition == 0:
+            if firstCdPastLine == True:
+                if int(marketInfo[m][-1]) > int(marketInfo[cdPastLineIndex][-1]):
+                    stopPosition = 1
+                    currentOperation[3] = currentOperation[2]
+            else:
+                firstCdPastLine = True
+                cdPastLineIndex = m
+        if int(marketInfo[m][-1]) > currentOperation[1] + (miSize*2*minBreakEvenDistance) and stopPosition == 1:
+            stopPosition = 2
+            currentOperation[3] += (miSize*minBreakEvenDistance) - 0.25*miSize
+        if int(marketInfo[m][-1]) > currentOperation[3] + (miSize*0.25) + (miSize*minBreakEvenDistance) and stopPosition == 2:
+            currentOperation[3] += miSize*minBreakEvenDistance
+
+    
     
 searchOperation()
