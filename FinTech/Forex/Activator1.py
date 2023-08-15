03d569eb73f84b3c43e759d53b9ca6488026b5e4
Ghiancarlo Morelli <ghian.m@gmail.com>
2023-08-14 23:03:18 -0300

    bug fixes

--- FinTech/Forex/Activator1.py
+++ FinTech/Forex/Activator1.py
@@ -9,7 +9,7 @@ Y_RANGE = [0.01,4.25]
 
 DEFINITION = 10
 
-for c in DEFINITION:
+for c in range(1, DEFINITION):
     X.append((X_RANGE[1]-X_RANGE[0]/DEFINITION)*c)
     Y.append((Y_RANGE[1]-Y_RANGE[0]/DEFINITION)*c)
 
