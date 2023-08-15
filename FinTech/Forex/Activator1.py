1643cb5e97f872e6df651cb34278108a3a9647f7
Ghiancarlo Morelli <ghian.m@gmail.com>
2023-08-15 12:05:44 -0300

    definition bug fixed

--- FinTech/Forex/Activator1.py
+++ FinTech/Forex/Activator1.py
@@ -1,5 +1,6 @@
 import ForexOperator1
 import time
+import math
 
 start_time = time.time()
 
@@ -12,11 +13,11 @@ bestOperation = []
 X_RANGE = [0.01,4.25]
 Y_RANGE = [0.01,4.25]
 
-DEFINITION = 100
+DEFINITION = 4 #insert here how many pixels of definition do you want for the 3d graph
 
-DEFINITION = int(DEFINITION/10)
-for l in range(1, DEFINITION):
-    for ll in range(1, DEFINITION):
+DEFINITION = int(math.sqrt(DEFINITION))
+for l in range(1, DEFINITION+1):
+    for ll in range(1, DEFINITION+1):
         X.append(((X_RANGE[1]-X_RANGE[0])/DEFINITION)*l)
         Y.append(((Y_RANGE[1]-Y_RANGE[0])/DEFINITION)*ll)
 dn=0
@@ -28,10 +29,10 @@ for c in range(0, len(X)):
         bestOperation = [X[c],Y[c],minZ]
     Z.append(fx)
     
-# print("X = "+ str(X))
-# print("Y = "+ str(Y))
-# print("Z = "+ str(Z))
-print(bestOperation)
+print("X = "+ str(X))
+print("Y = "+ str(Y))
+print("Z = "+ str(Z))
+# print(bestOperation)
 
 print(time.time()-start_time)
 
